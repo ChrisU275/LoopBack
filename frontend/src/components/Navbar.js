b1b4d822584b40f99e403cc8121a4f8f795b35a9
rahma-a1 <abdelarahma510@gmail.com>
2025-11-08 21:01:14 -0700

    marketplace frontend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,9 +5,35 @@ export default function Navbar(){
     <div className="nav" style={{ padding: '12px 0', height: '70px' }}>
   <div className="container" style={{display:'flex',gap:50,alignItems:'center',justifyContent:'left', height: '100%'}}>
     <nav style={{ fontSize: '30px', fontFamily: 'Coolvetica, sans-serif' }}>
-      <NavLink to="/marketplace" className={({isActive})=>isActive?'active':''}>marketplace</NavLink>
-      <NavLink to="/profile" className={({isActive})=>isActive?'active':''}>profile</NavLink>
-      <NavLink to="/about" className={({isActive})=>isActive?'active':''}>about</NavLink>
+      <NavLink 
+            to="/marketplace" 
+            className={({isActive})=>isActive?'active':''}
+            style={({isActive}) => ({
+              color: isActive ? '#F8BF65' : 'inherit'
+            })}
+          >
+            marketplace
+          </NavLink>
+
+            <NavLink 
+            to="/profile" 
+            className={({isActive})=>isActive?'active':''}
+            style={({isActive}) => ({
+              color: isActive ? '#E9708F' : 'inherit'
+            })}
+          >
+            profile
+          </NavLink>
+
+            <NavLink 
+            to="/about" 
+            className={({isActive})=>isActive?'active':''}
+            style={({isActive}) => ({
+              color: isActive ? '#8093CA' : 'inherit'
+            })}
+          >
+            about
+          </NavLink>
     </nav>
   </div>
 </div>
